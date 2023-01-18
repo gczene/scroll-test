df4fc804c2d1e0e4dbdadceba14a838d64f4acf3
Gabor Czene <gabor@czene.co.uk>
2023-01-18 10:13:41 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from "./components/footer";
+
+const arr = [...new Array(100)];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {arr.map((elem,idx) => {
+          return (<div key={'elem-' + idx} className={idx%2 === 0 ?'odd':'even'}>{idx} {navigator.userAgent}</div>)
+        })}
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,38 @@
+import React, {useEffect} from "react";
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+export default function Footer() {
+
+// modern Chrome requires { passive: false } when adding event
+
+
+  let supportsPassive = false;
+  try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+      get: function () { supportsPassive = true; }
+    }));
+  } catch(e) {};
+  const  wheelOpt = supportsPassive ? { passive: false } : false;
+  useEffect(() => {
+    setTimeout(() => {
+      window.addEventListener('touchmove', preventDefault, wheelOpt);
+      var body = document.getElementsByTagName('body');
+      body[0].style.overflow = 'hidden';
+      window?.scrollTo && window.scrollTo({ top: 0, behavior: 'smooth' });
+
+      window.addEventListener('touchend', () => {
+        window?.scrollTo && window.scrollTo({ top: 0, behavior: 'smooth' });
+      }, wheelOpt)
+
+    }, 3000);
+    return function () {
+      window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    }
+  }, [])
+  return (<>
+    footer
+  </>)
+}
