7eb227f91d64da53f8558f9e8585a824dd213ad6
Gabor Czene <gabor@czene.co.uk>
2023-01-19 14:25:05 +0000

    something

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ const arr = [...new Array(100)];
 function App() {
   return (
     <div className="App">
+      <div id={'fixed'} style={{position: 'fixed', top: 0, left: 0, backgroundColor: 'green', color: 'white'}}>HERE</div>
       <header className="App-header">
         {arr.map((elem,idx) => {
-          return (<div key={'elem-' + idx} className={idx%2 === 0 ?'odd':'even'}>{idx} {navigator.userAgent}</div>)
+          return (<div key={'elem-' + idx} className={(idx%2 === 0 ?'odd':'even')}>{idx} {navigator.userAgent}</div>)
         })}
       </header>
       <Footer />
--- src/components/footer.js
+++ src/components/footer.js
@@ -17,20 +17,49 @@ export default function Footer() {
     }));
   } catch(e) {};
   const  wheelOpt = supportsPassive ? { passive: false } : false;
-  useEffect(() => {
+  // document.addEventListener('scroll', (e) => {
+  //   e.preventDefault();
+  //   console.log(e)
+  // }, wheelOpt)
+
+  const TopScroll = window.pageYOffset || document.documentElement.scrollTop;
+  const LeftScroll = window.pageXOffset || document.documentElement.scrollLeft;
+  // window.onscroll = function() {
+  //   window.scrollTo(LeftScroll, TopScroll);
+  // };
+  // document.addEventListener('touchmove', preventDefault, wheelOpt);
+
+    useEffect(() => {
     setTimeout(() => {
-      window.addEventListener('touchmove', preventDefault, wheelOpt);
+      document.addEventListener('touchmove', () => {
+        document.getElementById('fixed').innerText = 'touchmoving'
+        // window?.scrollTo && window.scrollTo({ top: 0 });
+
+      });
       var body = document.getElementsByTagName('body');
-      body[0].style.overflow = 'hidden';
-      window.addEventListener('touchend', () => {
-        window?.scrollTo && window.scrollTo({ top: 0, behavior: 'smooth' });
-      }, wheelOpt)
+      body[0].style['overflow'] = 'hidden';
+      body[0].style.position = window.location.search ? 'fixed' : 'relative';
+      // const root = document.getElementById('root');
+      // root.style['overflow-x'] = 'hidden';
+      // root.style.position = 'relative';
+      document.getElementById('fixed').innerText = 'overflow becomes hidden';
+      ['touchend', 'touchmove', 'touchstart'].map((eventName) => {
+        document.addEventListener(eventName, () => {
+          window?.scrollTo && window.scrollTo({ top: 0, behavior: 'smooth' });
+        }, wheelOpt)
+      })
       window?.scrollTo && window.scrollTo({ top: 0, behavior: 'smooth' });
+      // window.onscroll = function() {
+      //   // window?.scrollTo && window.scrollTo({ top: 0, behavior: 'smooth' });
+      //   console.log(window.pageYOffset || document.documentElement.scrollTop)
+      //   // window.scrollTo(LeftScroll, TopScroll);
+      // };
 
 
     }, 3000);
     return function () {
-      window.removeEventListener('touchmove', preventDefault, wheelOpt);
+      window.onscroll = function() {};
+      document.removeEventListener('touchmove', preventDefault, wheelOpt);
     }
   }, [])
   return (<>
