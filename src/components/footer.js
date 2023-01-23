e25ac852b0d17155e96bf5ecd78ea4ff4b5578ce
Gabor Czene <gabor@czene.co.uk>
2023-01-23 07:40:07 +0000

    another version

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,9 @@
 /* eslint-disable */
 import React, {useEffect} from "react";
 
-function preventDefault(e) {
+function preventDefaultAndScrollUp(e) {
   e.preventDefault();
+  window?.scrollTo && window.scrollTo({ top: 0 });
 }
 
 export default function Footer() {
@@ -17,50 +18,26 @@ export default function Footer() {
     }));
   } catch(e) {};
   const  wheelOpt = supportsPassive ? { passive: false } : false;
-  // document.addEventListener('scroll', (e) => {
-  //   e.preventDefault();
-  //   console.log(e)
-  // }, wheelOpt)
-
-  const TopScroll = window.pageYOffset || document.documentElement.scrollTop;
-  const LeftScroll = window.pageXOffset || document.documentElement.scrollLeft;
-  // window.onscroll = function() {
-  //   window.scrollTo(LeftScroll, TopScroll);
-  // };
-  // document.addEventListener('touchmove', preventDefault, wheelOpt);
 
     useEffect(() => {
-    setTimeout(() => {
-      document.addEventListener('touchmove', () => {
-        document.getElementById('fixed').innerText = 'touchmoving'
-        // window?.scrollTo && window.scrollTo({ top: 0 });
-
-      });
-      var body = document.getElementsByTagName('body');
-      body[0].style['overflow'] = 'hidden';
-      body[0].style.position = window.location.search ? 'fixed' : 'relative';
-      // const root = document.getElementById('root');
-      // root.style['overflow-x'] = 'hidden';
-      // root.style.position = 'relative';
-      document.getElementById('fixed').innerText = 'overflow becomes hidden';
-      ['touchend', 'touchmove', 'touchstart'].map((eventName) => {
-        document.addEventListener(eventName, () => {
-          window?.scrollTo && window.scrollTo({ top: 0, behavior: 'smooth' });
-        }, wheelOpt)
-      })
-      window?.scrollTo && window.scrollTo({ top: 0, behavior: 'smooth' });
-      // window.onscroll = function() {
-      //   // window?.scrollTo && window.scrollTo({ top: 0, behavior: 'smooth' });
-      //   console.log(window.pageYOffset || document.documentElement.scrollTop)
-      //   // window.scrollTo(LeftScroll, TopScroll);
-      // };
+      setTimeout(() => {
+        const body = document.getElementsByTagName('body');
+        body[0].style['overflow'] = 'hidden';
+        if (window.screen.width < 500) {
+          body[0].style.position ='fixed';
+        }
+        window?.scrollTo && window.scrollTo({ top: 0, behavior: 'smooth' });
+        ['touchend', 'touchmove', 'touchstart', 'scroll'].map((eventName) => {
+          document.addEventListener(eventName, preventDefaultAndScrollUp, wheelOpt)
+        })
 
 
-    }, 3000);
-    return function () {
-      window.onscroll = function() {};
-      document.removeEventListener('touchmove', preventDefault, wheelOpt);
-    }
+      }, 3000);
+      return function () {
+        ['touchend', 'touchmove', 'touchstart', 'scroll'].map((eventName) => {
+          document.removeEventListener(eventName, preventDefaultAndScrollUp);
+        })
+      }
   }, [])
   return (<>
     footer
